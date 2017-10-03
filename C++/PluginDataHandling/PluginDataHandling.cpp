ce262cff018bd27e34cd9be1db9d51df7d9a4eb5
brianferguson <ferguson.brian@gmail.com>
2017-10-03 03:06:00 -0400

    Updated PluginDataHandling

--- C++/PluginDataHandling/PluginDataHandling.cpp
+++ C++/PluginDataHandling/PluginDataHandling.cpp
@@ -1,124 +1,118 @@
-/*
-Copyright (C) 2017 Trevor Hamilton
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+/* Copyright (C) 2017 Rainmeter Project Developers
+ *
+ * This Source Code Form is subject to the terms of the GNU General Public
+ * License; either version 2 of the License, or (at your option) any later
+ * version. If a copy of the GPL was not distributed with this file, You can
+ * obtain one at <https://www.gnu.org/licenses/gpl-2.0.html>. */
 
 #include <Windows.h>
 #include "../../API/RainmeterAPI.h"
 
+
 // Overview: This example demonstrates using both the data argument to keep data across
-// functions calls and also storing data in the Rainmeter.Data file for presistance across
-// skin reloads and even measures. In this example we will make a counter that counts the
-// number of updates that happens and saves them on Finalize to the Rainmeter.data
-// Note: You should never rely on Update happening at the exact time you specify
+// functions calls and also storing data in the |Rainmeter.data| file for presist access across
+// skin reloads and even measures. In this example we will make a counter that counts the number
+// of updates that happen and saves it when the skin unloads to the |Rainmeter.data| file.
+// Note: Remember that the Update function will not be called when the measure is paused or disabled.
 
-// Sample skin:
+//Sample skin:
 /*
-[Rainmeter]
-Update=1000
-DynamicWindowSize=1
-BackgroundMode=2
-SolidColor=255,255,255
+	[Rainmeter]
+	Update=1000
+	DynamicWindowSize=1
+	BackgroundMode=2
+	SolidColor=255,255,255
 
-[mCount]
-Measure=Plugin
-Plugin=DataHandling.dll
-StoreData=1
+	[mCount]
+	Measure=Plugin
+	Plugin=DataHandling
+	SaveData=1
 
-[TextCount]
-Meter=String
-MeasureName=mCount
-X=0
-Y=0R
-FontSize=20
-Text=%1
+	[mCountAt0]
+	Measure=Plugin
+	Plugin=DataHandling
+	StartingValue=0
 
-[mCountAt0]
-Measure=Plugin
-Plugin=DataHandling.dll
-StartingValue=0
+	[MeterCount]
+	Meter=String
+	MeasureName=mCount
 
-[TextCountAt0]
-Meter=String
-MeasureName=mCountAt0
-X=0
-Y=0R
-FontSize=20
-Text=%1
+	[MeterCountAt0]
+	Meter=String
+	MeasureName=mCountAt0
+	Y=5R
 */
 
 struct Measure
 {
-	Measure() {}
-	int myCounter = 0;
-	bool storeData = false;
+	int counter;
+	bool saveData;
+
+	LPCWSTR dataFile;
+
+	Measure() :
+		counter(0),
+		saveData(false),
+		dataFile(nullptr) {}
 };
 
-LPCWSTR PluginName = L"DataHandling"; //Always match this to your plugin name
-LPCWSTR KeyName = L"StoredCount"; //You can have multiple keys for your plugin that you store different info under
-const int MAXSIZE = 256;
 
 PLUGIN_EXPORT void Initialize(void** data, void* rm)
 {
 	Measure* measure = new Measure;
 	*data = measure;
+
+	// Get the path to the settings data file
+	measure->dataFile = RmGetSettingsFile();
 }
 
 PLUGIN_EXPORT void Reload(void* data, void* rm, double* maxValue)
 {
 	Measure* measure = (Measure*)data;
 
-	//Get starting value if one is defined
-	int startValue = RmReadInt(rm, L"StartingValue", -1);
+	// Note: If |DynamicVariables=1| is set on the measure, this function will get called
+	//  on every update cycle - meaning the counter values will use the starting value and
+	//  appear to *not* update at all.
 
-	//If string from option was null read from file
-	if (startValue == -1)
-	{
-		LPTSTR outString = new TCHAR[MAXSIZE];
+	// Get the starting value if one is defined
+	measure->counter = RmReadInt(rm, L"StartingValue", -1);
 
-		//Read from Rainmeter.data file, if no instace exists start at 0
-		GetPrivateProfileString(PluginName, KeyName, L"0", outString, MAXSIZE, RmGetSettingsFile());
-		measure->myCounter = _wtoi(outString);
-	}
-	else
+	// If |StartingValue| was not defined with with measure, read the |Count| from the data file
+	if (measure->counter < 0)
 	{
-		measure->myCounter = startValue;
+		// Get |Count| from the Rainmeter data file. If the key does not exist
+		//  in the data file, 0 will be used as the default value.
+		measure->counter = (int)GetPrivateProfileInt(L"Plugin_DataHandling", L"Count", 0, measure->dataFile);
 	}
-	//If store data is 1 then save data when measure is unloaded
-	measure->storeData = RmReadInt(rm, L"StoreData", 0) == 1 ? true : false;
+
+	// If the counter is to be saved in the data file, set SaveData=1 in the measure (in the
+	//  skin file).  The counter will be saved to the |Count| key in the data file when the
+	//  skin is unloaded or refreshed.
+	measure->saveData = RmReadInt(rm, L"SaveData", 0) == 1 ? true : false;
 }
 
 PLUGIN_EXPORT double Update(void* data)
 {
 	Measure* measure = (Measure*)data;
 
-	//Notice how the every measure has its own counter since it gets it from data
-	return ++measure->myCounter;
+	// Every measure will have its own counter separate from other measures.
+	// Increase the counter and reset if needed.
+	if (++measure->counter < 0)	measure->counter = 0;
+
+	return measure->counter;
 }
 
 PLUGIN_EXPORT void Finalize(void* data)
 {
 	Measure* measure = (Measure*)data;
 
-	//Note that in this example if multiple measures have store data set to true whichever unloads last's data will persist
-	if (measure->storeData)
+	if (measure->saveData)
 	{
-		wchar_t buffer[MAXSIZE];
-		wsprintfW(buffer, L"%d", measure->myCounter);
-		WritePrivateProfileString(PluginName, KeyName, buffer, RmGetSettingsFile());
+		// Note: In this example multiple |DataHandling| plugin measures (from any skin) will
+		//  overwrite this value. The counter from the last measure unloaded will persist.
+		WCHAR buffer[SHRT_MAX];
+		_itow_s(measure->counter, buffer, 10);
+		WritePrivateProfileString(L"Plugin_DataHandling", L"Count", buffer, measure->dataFile);
 	}
 
 	delete measure;
